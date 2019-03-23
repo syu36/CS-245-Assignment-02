6af5c358ad7518705432c8f84acedcae2495cb0d
Stephen Yu <syu36@usfca.edu>
2019-03-22 22:33:39 -0700

    External sort working, hybrid sort working

--- src/ExternalSort.java
+++ src/ExternalSort.java
@@ -4,38 +4,212 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
+/**
+ *
+ * Reads in data from a file and separates them into sorted chunks, placing them in a temp file. The temp files are then merged and sorted and written to an output file.
+ *
+ */
 public class ExternalSort {
 
+	/**
+	 * Sorts a set of data from a file and writes it out to an output file.
+	 * @param inputFile the input file
+	 * @param outputFile the output file
+	 * @param n the number of items in the file
+	 * @param k the amount of memory available
+	 * @throws IOException
+	 */
 	public void externalSort(String inputFile, String outputFile, int n, int k) throws IOException {
 		int chunk = 0;
-		int chunkSize = n / k;
 		Path input = Paths.get(inputFile);
 		Path output = Paths.get(outputFile);
 
+		// sort chunks and write to temp file
+		chunk = sortChunks(input, k);
+
+		// merge files
+		float[] merged = new float[n];
+		mergeChunks(merged, chunk);
+
+		// write out sorted data
+		writeSortedArray(merged, output);
+	}
+
+	/**
+	 * Merges the chunks (temp files) into one sorted array.
+	 * @param merged the sorted array
+	 * @param chunk the number of chunks
+	 * @throws IOException
+	 */
+	private void mergeChunks(float[] merged, int chunk) throws IOException {
+		float[] tempArray = new float[0];
+
+		// number of chunks is also number of temp files
+		for (int i = chunk; i > 0; i--) {
+			// use a count of items in the array to know size of temparray
+			int prevArrayCount = 0;
+
+			try (BufferedReader tempFile = Files.newBufferedReader(Paths.get("temp" + (chunk < 10 ? "0" + chunk-- : chunk--)))) {
+				int mergedi = 0;
+				int tempi = 0;
+
+				String fileItem = tempFile.readLine();
+
+				// Basically use merge algorithm, temparray is the left array and the tempFile is the right array
+				while (tempi < tempArray.length && fileItem != null) {
+					float item = Float.parseFloat(fileItem);
+
+					if (tempArray[tempi] <= item) {
+						merged[mergedi++] = tempArray[tempi++];
+					} else {
+						merged[mergedi++] = item;
+						fileItem = tempFile.readLine();
+					}
+
+					prevArrayCount++;
+				}
+
+				while (tempi < tempArray.length) {
+					merged[mergedi++] = tempArray[tempi++];
+					prevArrayCount++;
+				}
+
+				while (fileItem != null) {
+					float item = Float.parseFloat(fileItem);
+					merged[mergedi++] = item;
+					fileItem = tempFile.readLine();
+					prevArrayCount++;
+				}
+			}
+			// Set tempArray to all data in merged but not as large as merged is. This allows us to merge each file into the merged array
+			tempArray = Arrays.copyOf(merged, prevArrayCount);
+		}
+	}
+
+	/**
+	 * Splits the input data into chunks and sorts them, then writes those chunks out to a tempfile.
+	 * @param input the input file
+	 * @param k the amount of available memory
+	 * @return the number of chunks
+	 * @throws IOException
+	 */
+	private int sortChunks(Path input, int k) throws IOException {
+		int chunk = 0;
+
+		// Read input, sort into chunks, place in temp files
 		try (BufferedReader reader = Files.newBufferedReader(input)) {
 			String line = reader.readLine();
 			while (line != null) {
-				float arr[] = new float[chunkSize];
 
-				for (int i = 0; i < chunkSize; i++) {
-					arr[i] = Float.parseFloat(line);
+				float arr[] = new float[k];
+
+				for (int i = 0; i < k; i++) {
+
+					try {
+						arr[i] = Float.parseFloat(line);
+						System.out.println(line);
+					} catch (NullPointerException e){
+						// turns arr into a new array that is the size of the remaining elements in last chunk
+						arr = Arrays.copyOf(arr, i);
+						// set i to k to get out of loop
+						i = k;
+					}
 					line = reader.readLine();
 				}
 
-				//sort(arr);
+				// Use mergesort for sorting chunks
+				mergeSort(arr);
 
-				try (BufferedWriter writer = Files.newBufferedWriter(Paths.get("temp" + (chunk < 10 ? "0" + ++chunk : ++chunk)))) {
+				try (BufferedWriter writer = Files.newBufferedWriter(Paths.get("temp" + (chunk < 9 ? "0" + ++chunk : ++chunk)))) {
 					for (float element : arr) {
 						writer.write(element + "\n");
 					}
 				}
 			}
 		}
+		return chunk;
+	}
+
+	/**
+	 * Writes the sorted data out to the output file.
+	 * @param arr the sorted array
+	 * @param output the output file
+	 * @throws IOException
+	 */
+	private void writeSortedArray(float[] arr, Path output) throws IOException {
+		try (BufferedWriter writer = Files.newBufferedWriter(output)) {
+			for (float item : arr) {
+				writer.write(Float.toString(item));
+				writer.newLine();
+			}
+		}
+	}
+
+	/**
+	 * Performs a merge sort.
+	 * @param a the array to merge sort
+	 */
+	private void mergeSort(float[] a) {
+		float[] left = getLeft(a);
+		float[] right = getRight(a);
+		if (left.length > 1) {
+			mergeSort(left);
+		}
+		if (right.length > 1) {
+			mergeSort(right);
+		}
+		merge(a, left, right);
+	}
+
+	/**
+	 * Merges two arrays together, sorting as they merge
+	 * @param array the array to sort
+	 * @param left the left split of the array
+	 * @param right the right split of the array
+	 */
+	private void merge(float[] array, float[] left, float[] right) {
+		int lefti = 0;
+		int righti = 0;
+		int arrayi = 0;
+		while (lefti < left.length && righti < right.length) {
+			if(left[lefti] <= right[righti]) {
+				array[arrayi++] = left[lefti++];
+			} else {
+				array[arrayi++] = right[righti++];
+			}
+		}
+
+		while (lefti < left.length) {
+			array[arrayi++] = left[lefti++];
+		}
+
+		while (righti < right.length) {
+			array[arrayi++] = right[righti++];
+		}
+	}
+
+	/**
+	 * Helper method to return the left half of an array
+	 * @param array the array
+	 * @return the right half of the array
+	 */
+	private float[] getLeft(float[] array) {
+		return Arrays.copyOfRange(array, 0, array.length / 2);
+	}
+
+	/**
+	 * Helper method to return the right half of an array
+	 * @param array the array
+	 * @return the right half of the array
+	 */
+	private float[] getRight(float[] array) {
+		return Arrays.copyOfRange(array, array.length / 2, array.length);
 	}
 
 	public static void main(String args[]) throws IOException {
 		ExternalSort sort = new ExternalSort();
-		sort.externalSort("input", "output", 4, 2);
+		sort.externalSort("input", "output", 30, 4);
 	}
 }
--- src/HybridSort.java
+++ src/HybridSort.java
@@ -1,24 +1,74 @@
 import java.util.Random;
 
 public class HybridSort {
-
-	private int quicksort (int[] arr, int left, int right) {
-		// base case
-		if (left < right) {
-			int p = partition(arr, left, right);
-			quicksort(arr, left, p - 1);
-			quicksort(arr, p + 1, right);
+	/**
+	 * Performs a hybrid sort. Uses quick sort for large arrays and insertion for small arrays.
+	 * @param arr the array
+	 * @param left the left index of the subarray
+	 * @param right the right index of the subarray
+	 */
+	public void hybridSort(double []arr, int left, int right) {
+		// Use 10 as changing point for sort algorithm
+		if (right - left > 10) {
+			quickSort(arr, left, right);
+		} else {
+			quadraticSort(arr, left, right);
 		}
-
-		return -1;
 	}
 
-	private int partition (int[] arr, int left, int right) {
+	/**
+	 * Performs an insertion sort on the array.
+	 * @param arr the array
+	 * @param left the left index of the subarray
+	 * @param right the right index of the subarray
+	 */
+	private void quadraticSort(double []arr, int left, int right) {
+		for (int i = 1; i < arr.length; i++) {
+			double temp = arr[i];
+			int j = i - 1;
+
+			while (j >= 0 && arr[j] > temp) {
+				arr[j + 1] = arr[j];
+				j--;
+			}
+
+			arr[j + 1] = temp;
+		}
+	}
+
+	/**
+	 * Performs a quick sort on the array.
+	 * @param arr the array
+	 * @param left the left index of the subarray
+	 * @param right the right index of the subarray
+	 * @return the index of the pivot after sorting
+	 */
+	private int quickSort (double[] arr, int left, int right) {
+		int p = -1;
+		// base case
+		if (left < right) {
+			p = partition(arr, left, right);
+			hybridSort(arr, left, p - 1);
+			hybridSort(arr, p + 1, right);
+		}
+		return p;
+	}
+
+	/**
+	 * Performs a partition on the array.
+	 * @param arr the array
+	 * @param left the left index of the subarray
+	 * @param right the right index of the subarray
+	 * @return the index of the pivot after sorting
+	 */
+	private int partition (double[] arr, int left, int right) {
 		if (left < right) {
 			Random random = new Random();
 			int pivot = random.nextInt(right - left + 1) + left; // random in the range of left to right (subarray indices)
+
 			// swap the pivot with the left most index so we can use the same partition code with slight adjustment
 			swap(arr, pivot, left);
+
 			pivot = left;
 			int i = left + 1;
 			int j = right;
@@ -46,20 +96,24 @@ public class HybridSort {
 		return left;
 	}
 
-	private void swap(int[] a, int i, int j) {
-		int temp = a[i];
+	/**
+	 * Swaps two elements of an array
+	 * @param a the array
+	 * @param i the first element
+	 * @param j the second element
+	 */
+	private void swap(double[] a, int i, int j) {
+		double temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
 	}
 
 	public static void main (String []args) {
 		HybridSort sort = new HybridSort();
-		int[] arr = new int[] {4, 23, 6, 18, 22, 5, 10, 2, 8, 11};
-		sort.quicksort(arr, 0, arr.length - 1);
-		System.out.println("Final");
-		for (int element : arr) {
+		double[] arr = new double[] {4.2, 23, 6, 18, 22, 5, 10, 2, 8, 11, 12, 15, 203, 234, 5346, 235, 23154, 231, 453};
+		sort.hybridSort(arr, 0, arr.length - 1);
+		for (double element : arr) {
 			System.out.print(element + ",");
 		}
-
 	}
 }
